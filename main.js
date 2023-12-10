220e8f1d30481f056c2490420a3380196247d26b
Aranza Quijano <91640852+araloul@users.noreply.github.com>
2023-12-09 22:18:14 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -3,12 +3,12 @@ let app = document.getElementById('typewriter');
 let typewriter = new Typewriter(app, {
   loop: true,
   delay: 75,
- cursor: "<span style='color: #fb4993;'>|</span>",
+ cursor: "<span style='color: #ad30de;'>|</span>",
 });
  
 typewriter
   .pauseFor(2500)
-  .typeString('<span style="color: #fb4993;">Desarrolladora Frontend</span>')
-  .pauseFor(200)
-  .deleteChars(10)
+  .typeString('<span style="color: ;">Diseñadora y Desarrolladora Front-end</span>')
+  .pauseFor(400)
+  .deleteChars(20)
   .start();
